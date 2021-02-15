18181486ef1a6a02e140c61151b14420d80f747b
JWalters <joshwalters34@gmail.com>
2021-02-15 09:12:26 -0500

    added more questions to html and displaying them with javascript.

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,11 @@ var scoreHeader = document.querySelector("#scoreHeader");
 var goBackBtn = document.querySelector("#goBack");
 var clearBtn = document.querySelector("#clear");
 var scoreWindow = document.querySelector("#highScore");
+var qOneEl = document.querySelector("#question1");
+var answerOneBtn = document.querySelector("#answer1Btn");
 
 
+// to hide elements when page loads
 window.onload = function () {
   document.getElementById("highScore").style.visibility = 'hidden';
   document.getElementById("scoreHeader").style.visibility = 'hidden';
@@ -20,8 +23,9 @@ window.onload = function () {
   document.getElementById("question3").style.visibility = "hidden";
   document.getElementById("question4").style.visibility = "hidden";
 }
-var timerInterval;
 
+var timerInterval;
+// start butten event listener to start time and get first question
 startBtn.addEventListener("click", function() {
   startGame();
 }
@@ -36,25 +40,26 @@ function startGame() {
     timerEl.textContent = secondsLeft;
 
     if(secondsLeft === 0) {
-      mainEl.textContent = "Game Over.  Better luck next time"
+      window.alert("Game Over.  Better luck next time!");
+      location.reload(); //reloads the page after clicking OK on prompt
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
       // sendMessage(); 
     }
   }, 1000);
+  // hiding the main page elements to display questions
   mainEl.style.display = "none";
   scoreEl.style.display = "none";
   newScoreEl.style.display = "none";
   startBtn.style.display = "none";
-
-  document.getElementById("question1").style.visibility = "visible";
+// making question 1 visible
+  document.getElementById("question1").style.visibility = "visible"; 
 }
-
+// event listner added to "View High Scores" to show high score
 highScoreEl.addEventListener("click", function() {
   scores ();
-}
-)
+})
 
 function scores(){
   mainEl.style.display = "none";
@@ -75,12 +80,15 @@ function scores(){
     scoreWindow.style.visibility = "visible";
   }
 }
-
+// event listener on Go Back button to take user back to start page
 goBackBtn.addEventListener("click", function() {
-  goBack(); 
-})
-
-function goBack() {
   location.reload();
+})
+// event listerner for answer to question 1
+answerOneBtn.addEventListener("click", function(){
+  questionTwo();
+})
+function questionTwo() {
+  document.getElementById("question1").style.display = "none"; 
+  document.getElementById("question2").style.visibility = "visible"; 
 }
-
